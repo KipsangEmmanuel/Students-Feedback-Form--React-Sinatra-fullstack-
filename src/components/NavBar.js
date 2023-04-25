a68798844073d130b71f24192855498f95a3711a
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-24 22:11:10 -0700

    initialised the app

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import "../css/NavBar.css"
+function NavBar() {
+    return (
+        <>
+            <nav className="container">
+                <a href="">Students</a>
+                <a href="">Responses</a>
+                <a href="">Settings</a>
+            </nav>
+
+        </>
+    )
+}
+export default NavBar;
