36b4f332f3f240ce6bdda3c8ade494e4470c3361
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-24 22:35:42 -0700

    added Form component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import NavBar  from "./components/NavBar";
+import Form from "./components/Form";
 function App() {
   return (
     <div>
       <NavBar/>
+      <Form />
 
     </div>
   );
---
+++ src/components/Form.js
@@ -0,0 +1,25 @@
+import "../css/Form.css"
+function Form () {
+    return(
+        <>
+          <form className="container">
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">Full Name</label>
+              <input type="text" id="name" className="form-control" required/>
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email Address</label>
+              <input type="email" id="email" className="form-control" required/>
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="course" className="form-label">Course</label>
+              <input type="text" id="course" className="form-control" required/>
+            </div>
+            <button className="btn">Submit</button>
+          </form>
+        </>
+    )
+}
+export default Form;
