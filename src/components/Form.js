699d470480b3c75a16584a446025566134a8a3bc
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-26 15:47:36 -0700

    added cuptureInput func

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,52 @@
+import { useState } from "react";
 import "../css/Form.css"
 function Form () {
+   const [formData, setFormData] = useState({
+     name: "",
+     email: "",
+     course: ""
+   })
+   // cupture the user input values
+
+   function captureInput(event) {
+     event.preventDefault()
+     setFormData({...formData, [event.target.id]: event.target.value});
+   }
 
     return(
         <>
           <form className="container">
             <div className="mb-3">
               <label htmlFor="name" className="form-label">Full Name</label>
-              <input type="text" id="name" className="form-control" required/>
+              <input 
+                type="text" 
+                id="name" 
+                className="form-control" 
+                value={formData.name} 
+                onChange={captureInput}  
+                required/>
             </div>
 
             <div className="mb-3">
               <label htmlFor="email" className="form-label">Email Address</label>
-              <input type="email" id="email" className="form-control" required/>
+              <input 
+              type="email" 
+              id="email"
+              value={formData.email} 
+              onChange={captureInput}
+              className="form-control" 
+              required/>
             </div>
 
             <div className="mb-3">
               <label htmlFor="course" className="form-label">Course</label>
-              <input type="text" id="course" className="form-control" required/>
+              <input 
+              type="text" 
+              id="course" 
+              value={formData.course}
+              onChange={captureInput}
+              className="form-control" 
+              required/>
             </div>
             <button className="btn">Submit</button>
           </form>
