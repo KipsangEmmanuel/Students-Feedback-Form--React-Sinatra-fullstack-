a0ee0fe8545c53dd179e50a2395024724cd68a9a
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-04-28 17:46:54 -0700

    added the routes compinents

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import NavBar  from "./components/NavBar";
 import Form from "./components/Form";
 import Table from "./components/Table";
 import TableRaw from "./components/Student";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div>
+      
+      <BrowserRouter>
       <NavBar/>
-      <Form />
+      <Routes>
+        <Route path="/students" element={<Table />}></Route>
+        <Route path="/response" element={<Form />}></Route>
+        <Route path="/" element={<Table />}></Route>
+
+      </Routes>
+      </BrowserRouter>
+      {/* <Form />
       <Table />
-      <TableRaw />
+      <TableRaw /> */}
 
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,23 @@
+import { NavLink } from "react-router-dom";
 import "../css/NavBar.css"
 function NavBar() {
+
+    //I passed <a> tag to the NavLink className because I had already styled the <a> tag
     return (
         <>
-            <nav className="container">
-                <a href="">Students</a>
+            <nav className="container"> 
+                <NavLink className={"a"} to="/students" end>
+                    Students
+                </NavLink>
+                <NavLink className={"a"} to="/response" end>
+                    Response
+                </NavLink>
+                <NavLink className={"a"} to="/" end>
+                    Settings
+                </NavLink>
+                {/* <a href="">Students</a>
                 <a href="">Responses</a>
-                <a href="">Settings</a>
+                <a href="">Settings</a> */}
             </nav>
 
         </>
